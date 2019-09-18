0bf9c66a08fc97f565f723eb30eb36b209b89612
Ariel Noyman <noyman@mit.edu>
2019-09-18 13:31:04 -0400

    deploy to gh without dist

--- deploy2gh.sh
+++ deploy2gh.sh
@@ -2,10 +2,12 @@
 # clear dist folder 
 sudo rm -rf dist
 # build the dist for public url 
-sudo parcel build index.html --public-url https://cityscope.media.mit.edu/CS_CityScopeJS/
+sudo parcel build index.html --public-url https://cityscope.media.mit.edu/CS_cityscopeJS/
 # make sure to add dist to commit if .gitignored 
 git add dist -f
 #commit the GH pages changes 
 git commit -m "gh-pages commit"
 #push to subtree remote [Force and remove all] 
 git push origin `git subtree split --prefix dist master`:gh-pages --force
+# finally remove dist from git 
+sudo git rm -r --cached --ignore-unmatch dist
