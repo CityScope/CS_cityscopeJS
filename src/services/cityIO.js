e1e213b5d218c8c33b3a16300e078820727c0759
Ariel Noyman <noyman@mit.edu>
2019-12-15 15:44:38 -0500

    cityio called via axios for 502 handling

--- src/services/cityIO.js
+++ src/services/cityIO.js
@@ -1,9 +1,33 @@
+// export const getCityIO = URL =>
+//     fetch(URL)
+//         .then(response => response.json())
+//         .then(result => {
+//             return result;
+//         })
+//         .catch(e => {
+//             console.log(e);
+//             return e;
+//         });
+
+// const axios = require("axios");
+
+import axios from "axios";
+
 export const getCityIO = URL =>
-    fetch(URL)
-        .then(response => response.json())
-        .then(result => {
-            return result;
+    axios
+        .get(URL)
+        .then(response => {
+            console.log("response:", response);
         })
-        .catch(e => {
-            console.log(e);
+        .catch(error => {
+            if (error.response) {
+                console.log("error.response:", "\n", error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log("error.request:", error.request);
+            } else {
+                console.log("misc error:", error.message);
+            }
+            console.log("request config:", error.config);
         });
