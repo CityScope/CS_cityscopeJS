25f95a8298c34e803deaeba7e6282dc5dea25919
Ariel Noyman <noyman@mit.edu>
2019-12-15 16:50:44 -0500

    placeholder json for testing

--- src/services/cityIO.js
+++ src/services/cityIO.js
@@ -1,29 +1,22 @@
-// export const getCityIO = URL =>
-//     fetch(URL)
-//         .then(response => response.json())
-//         .then(result => {
-//             return result;
-//         })
-//         .catch(e => {
-//             console.log(e);
-//             return e;
-//         });
-
-// const axios = require("axios");
-
 import axios from "axios";
 
 export const getCityIO = URL =>
     axios
         .get(URL)
         .then(response => {
-            console.log("response:", response);
+            return response.data;
         })
         .catch(error => {
             if (error.response) {
-                console.log("error.response:", "\n", error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
+                console.log(
+                    "error.response:",
+                    "\n",
+                    error.response.data,
+                    "\n",
+                    error.response.status,
+                    "\n",
+                    error.response.headers
+                );
             } else if (error.request) {
                 console.log("error.request:", error.request);
             } else {
