d27cf8bb140b963f372796f101c99e66a1fa35c4
Ariel Noyman <noyman@mit.edu>
2020-01-27 09:57:15 -0500

    docs WIP

--- src/docs/docs.js
+++ src/docs/docs.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
 import "./docs.css";
+import axios from "axios";
 const ReactMarkdown = require("react-markdown");
-
-// const arch = require("./md/arch.md");
-// const home = require("./md/home.md");
-// const schema = require("./md/schema.md");
 
 class Docs extends Component {
     state = { text: "loading..." };
 
     componentDidMount() {
-        const doc = require("./md/" + this.props.doc + ".md");
-        fetch(doc)
-            .then(response => response.text())
-            .then(text => this.setState({ text: text }));
+        const doc =
+            "https://raw.githubusercontent.com/CityScope/CS_cityscopeJS/master/src/docs/md/" +
+            this.props.doc +
+            ".md";
+
+        axios
+            .get(doc, {
+                mode: "no-cors"
+            })
+            .then(response => {
+                this.setState({ text: response.data });
+            });
     }
 
     render() {
