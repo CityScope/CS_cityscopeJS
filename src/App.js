7a74ba5ce0f87752e1261f8683d3bcfde5c36edc
Ariel Noyman <noyman@mit.edu>
2020-06-01 11:37:40 -0400

    fix URI redircet error

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import configureStore from "./redux/store";
 import Screen from "./components/Screen";
 import { StylesProvider } from "@material-ui/core/styles";
 import React, { Component } from "react";
+import { newDataStyle } from "./services/consoleStyle";
 
-// redux store configuration
 const store = configureStore();
 
 const MapRoute = () => {
-    let urlArray = window.location.href.split("/");
-    let projectName = urlArray[urlArray.length - 1];
+    let url = window.location.toString();
+    let pre = "cityscope=";
+    let cityscopePrjName = url.substring(url.indexOf(pre) + pre.length);
+
     let table = null;
-    if (projectName) {
-        table = projectName;
+    if (url.indexOf(pre) !== -1 && cityscopePrjName.length > 0) {
+        table = cityscopePrjName;
     } else {
         table = "grasbrook";
     }
+    console.log("%c loading CityScope project: " + table, newDataStyle);
 
     return (
         <Provider store={store}>
