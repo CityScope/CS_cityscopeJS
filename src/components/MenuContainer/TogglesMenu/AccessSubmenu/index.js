8a9b762ff10dc028591c94496ed94667bce83c0f
Ariel Noyman <noyman@mit.edu>
2020-06-17 22:08:53 -0400

    heatmap legend, closes #84

--- src/components/MenuContainer/TogglesMenu/AccessSubmenu/index.js
+++ src/components/MenuContainer/TogglesMenu/AccessSubmenu/index.js
@@ -6,6 +6,9 @@ import ListItemText from "@material-ui/core/ListItemText";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import { listenToAccessToggle } from "../../../../redux/actions";
 import Switch from "@material-ui/core/Switch";
+import { ContinuousColorLegend } from "react-vis";
+
+import settings from "../../../../settings/settings.json";
 
 function AccessSubmenu(props) {
     const accessToggle = useSelector((state) => state.ACCESS_TOGGLE);
@@ -18,9 +21,24 @@ function AccessSubmenu(props) {
             : [];
 
     const dispatch = useDispatch();
-
+    const c = settings.map.layers.heatmap.colors;
+    const l = c.length - 1;
+    const m = Math.floor(c.length - 1);
     return (
         <List>
+            <ListItem>
+                <ContinuousColorLegend
+                    width={500}
+                    startColor={
+                        "rgb(" + c[0][0] + "," + c[0][1] + "," + c[0][2] + ")"
+                    }
+                    endColor={
+                        "rgb(" + c[l][0] + "," + c[l][1] + "," + c[l][2] + ")"
+                    }
+                    startTitle="No Access"
+                    endTitle="Full Access"
+                />
+            </ListItem>
             {ACCESS_PROPERTIES.map((p, i) => (
                 <ListItem key={p}>
                     <ListItemText
