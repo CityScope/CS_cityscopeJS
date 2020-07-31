6f726e1e2270bd39a98e7862eeea5b49f07f6fe5
Ariel Noyman <noyman@mit.edu>
2020-07-31 10:46:06 -0400

    Updates

---
+++ precache-manifest.e0b83a5e0083a97eca4fc7bd40ec46da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac7ed985b8eb52d5bc5ae4db6d604af",
+    "url": "https://cityscope.media.mit.edu/CS_cityscopeJS/index.html"
+  },
+  {
+    "revision": "ca1515fc72c02e08d6af",
+    "url": "https://cityscope.media.mit.edu/CS_cityscopeJS/static/css/2.23bc2e7b.chunk.css"
+  },
+  {
+    "revision": "f5a695916bdd1f548e3b",
+    "url": "https://cityscope.media.mit.edu/CS_cityscopeJS/static/css/main.7109ebba.chunk.css"
+  },
+  {
+    "revision": "ca1515fc72c02e08d6af",
+    "url": "https://cityscope.media.mit.edu/CS_cityscopeJS/static/js/2.f634f1cf.chunk.js"
+  },
+  {
+    "revision": "c42c08ebbceb3bb233d4dc73665774ec",
+    "url": "https://cityscope.media.mit.edu/CS_cityscopeJS/static/js/2.f634f1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a695916bdd1f548e3b",
+    "url": "https://cityscope.media.mit.edu/CS_cityscopeJS/static/js/main.8efa3778.chunk.js"
+  },
+  {
+    "revision": "a1beac5cb4679259f6cb",
+    "url": "https://cityscope.media.mit.edu/CS_cityscopeJS/static/js/runtime-main.be45fd82.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://cityscope.media.mit.edu/CS_cityscopeJS/precache-manifest.670b95f84d199741d351da479a5525d1.js"
+  "https://cityscope.media.mit.edu/CS_cityscopeJS/precache-manifest.e0b83a5e0083a97eca4fc7bd40ec46da.js"
 );
 
 self.addEventListener('message', (event) => {
