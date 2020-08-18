a62d6234303067cbe7e183d40b956d68ed40ebd4
Ariel Noyman <noyman@mit.edu>
2020-08-18 13:30:54 -0400

    extract functions from useEffect

--- src/components/CityScopeJS/CityIO/cityIO.js
+++ src/components/CityScopeJS/CityIO/cityIO.js
@@ -32,74 +32,67 @@ export default function CityIO(props) {
      * start fetching API hashes to check for new data
      */
     useEffect(() => {
-        const timer = setTimeout(async function update() {
-            const newHashId = await getAPICall(cityioURL + "/meta/id");
-            if (hashId !== newHashId) {
-                setHashId(newHashId);
-            }
-            // recursively call setTimeout when api call completes
-            setTimeout(update, settings.cityIO.interval);
-        }, settings.cityIO.interval);
-
-        console.log(
-            "starting cityIO GET interval every " +
-                settings.cityIO.interval +
-                "ms "
-        );
-
+        const timer = setTimeout(update, settings.cityIO.interval);
+        console.log("reading cityIO every" + settings.cityIO.interval + "ms");
         return () => clearTimeout(timer);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    async function update() {
+        // recursively get hashes
+        const newHashId = await getAPICall(cityioURL + "/meta/id");
+        if (hashId !== newHashId) {
+            setHashId(newHashId);
+        }
+        setTimeout(update, settings.cityIO.interval);
+    }
+
+    async function getModules() {
+        const newHashes = await getAPICall(cityioURL + "/meta/hashes");
+        const promises = [];
+        const pickedModules = settings.cityIO.cityIOmodules;
+        // for each of the modules in settings, add api call to promises
+        pickedModules.forEach((module) => {
+            if (hashes[module] !== newHashes[module]) {
+                promises.push(getAPICall(cityioURL + "/" + module));
+            } else {
+                promises.push(null);
+            }
+        });
+        const modules = await Promise.all(promises);
+        setHashes(newHashes);
+
+        // update cityio object with modules data
+        const modulesData = pickedModules.reduce((obj, k, i) => {
+            if (modules[i]) {
+                console.log(`updating ${k}`);
+                return { ...obj, [k]: modules[i] };
+            } else {
+                return obj;
+            }
+        }, cityioData);
+        modulesData.tableName = tableName;
+
+        // send to cityio
+        dispatch(getCityioData(modulesData));
+        console.log("done updating from cityIO");
+
+        // initializes rendering of Menu and Map containers
+        dispatch(setReadyState(true));
+        dispatch(setLoadingState(false));
+    }
+
     useEffect(() => {
-        // only update if hashId changes
+        //! only update if hashId changes
         if (!hashId) {
             return;
         }
-
         // reset the state of loading flag
         dispatch(setLoadingState(true));
-
         // set Scenario Names and Ids
         getScenarioIndices(tableName, (data) =>
             dispatch(setScenarioNames(data))
         );
-
-        async function getModules() {
-            const newHashes = await getAPICall(cityioURL + "/meta/hashes");
-
-            const promises = [];
-            const pickedModules = settings.cityIO.cityIOmodules;
-            // for each of the modules in settings, add api call to promises
-            pickedModules.forEach((module) => {
-                if (hashes[module] !== newHashes[module]) {
-                    promises.push(getAPICall(cityioURL + "/" + module));
-                } else {
-                    promises.push(null);
-                }
-            });
-            const modules = await Promise.all(promises);
-            setHashes(newHashes);
-
-            // update cityio object with modules data
-            const modulesData = pickedModules.reduce((obj, k, i) => {
-                if (modules[i]) {
-                    console.log(`updating ${k}`);
-                    return { ...obj, [k]: modules[i] };
-                } else {
-                    return obj;
-                }
-            }, cityioData);
-            modulesData.tableName = tableName;
-
-            // send to cityio
-            dispatch(getCityioData(modulesData));
-            console.log("done updating from cityIO");
-
-            // initializes rendering of Menu and Map containers
-            dispatch(setReadyState(true));
-            dispatch(setLoadingState(false));
-        }
         getModules();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [hashId]);
