49b7829252dacb17d3505ddad28cc539c615effb
ayeshaali <aliayeshanyc@gmail.com>
2021-01-13 12:35:39 -0500

    fixed translate boundary error

--- src/components/CityScopeJS/BaseMap/layers/RoboscopeGridLayer.js
+++ src/components/CityScopeJS/BaseMap/layers/RoboscopeGridLayer.js
@@ -21,8 +21,8 @@ export const translate = (data, features, start, end) => {
   const [x2,y2] = twoDcoords(data, end);
   const [x_f, y_f] = twoDcoords(data, features[0]);
   const [x_l, y_l] = twoDcoords(data, features[features.length-1]);
-  const change_y= (y2-y1>0) ? Math.min(y2-y1, data.nrows-y_l) : Math.max(y2-y1, 0-y_f)
-  const change_x= (x2-x1 > 0) ? Math.min(x2-x1, data.ncols-x_l) : Math.max(x2-x1, 0-x_f)  
+  const change_y= (y2-y1>0) ? Math.min(y2-y1, data.nrows-y_l-1) : Math.max(y2-y1, 0-y_f)
+  const change_x= (x2-x1 > 0) ? Math.min(x2-x1, data.ncols-x_l-1) : Math.max(x2-x1, 0-x_f)  
   features.forEach((item) => {
     var [x, y] = twoDcoords(data, item);
     output.push(flat_index(data, x+change_x, y+change_y));
