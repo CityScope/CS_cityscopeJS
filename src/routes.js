f792952d3d13f3b432435fe4fd3efab1267de9b8
Ariel Noyman <noyman@mit.edu>
2021-04-03 22:32:20 -0400

    temp routing

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,27 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import MainLayout from "./layouts/MainLayout";
-import NotFoundView from "./views/Errors/NotFoundView";
-import GridEditor from "./views/GridEditor";
-import CityScopeJS from "./views/CityScopeJS";
-import ProjectionMapping from "./views/ProjectionMapping";
-import CityIOviewer from "./views/CityIOviewer";
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import NotFoundView from './views/Errors/NotFoundView'
+import GridEditor from './views/GridEditor'
+import CityScopeJS from './views/CityScopeJS'
+import ProjectionMapping from './views/ProjectionMapping'
+import CityIOviewer from './views/CityIOviewer'
 
 const routes = [
-    {
-        path: "/",
-        element: <MainLayout />,
-        children: [
-            { path: "/home", element: <CityIOviewer /> },
-            { path: "/", element: <CityScopeJS /> },
-            { path: "/editor", element: <GridEditor /> },
-            { path: "/projection", element: <ProjectionMapping /> },
-            { path: "/cityioviewer", element: <CityIOviewer /> },
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      { path: '/home', element: <CityIOviewer /> },
+      { path: '/', element: <CityScopeJS /> },
+      { path: '/editor', element: <GridEditor /> },
+      { path: '/projection', element: <ProjectionMapping /> },
+      { path: '/cityioviewer', element: <Navigate to="/home" /> },
 
-            { path: "*", element: <Navigate to="/404" /> },
-            { path: "404", element: <NotFoundView /> },
-        ],
-    },
-];
+      { path: '*', element: <Navigate to="/404" /> },
+      { path: '404', element: <NotFoundView /> },
+    ],
+  },
+]
 
-export default routes;
+export default routes
