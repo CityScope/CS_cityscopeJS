24d09e7fe7ed2d60ca157b971268442424ae8253
Ariel Noyman <arielnoyman@gmail.com>
2021-05-25 13:05:11 -0400

    remove grid maker + proj mapping for now

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import { Navigate } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import NotFoundView from './views/Errors/NotFoundView'
-import GridEditor from './views/GridEditor'
+// import GridEditor from './views/GridEditor'
 import CityScopeJS from './views/CityScopeJS'
-import ProjectionMapping from './views/ProjectionMapping'
+// import ProjectionMapping from './views/ProjectionMapping'
 
 const routes = [
   {
@@ -11,8 +11,8 @@ const routes = [
     element: <MainLayout />,
     children: [
       { path: '/', element: <CityScopeJS /> },
-      { path: '/editor/', element: <GridEditor /> },
-      { path: '/projection', element: <ProjectionMapping /> },
+      // { path: '/editor/', element: <GridEditor /> },
+      // { path: '/projection', element: <ProjectionMapping /> },
       { path: '/cityioviewer', element: <Navigate to="/" /> },
 
       { path: '*', element: <Navigate to="/404" /> },
