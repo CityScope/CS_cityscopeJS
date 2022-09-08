ad80db20385d5fae8c93fbf4e6ee98e68c7ad93c
Ariel Noyman <arielnoyman@gmail.com>
2022-09-08 13:56:05 -0400

    fix anim reduces

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from "redux";
 import cityIOdataSliceReducer from "./cityIOdataSlice";
 import menuSliceReducer from "./menuSlice";
 import editorMenuSliceReducer from "./editorMenuSlice";
-import animationSliceReducer from "../../Components/_wip_animation/_animationSlice"; 
 
 export default combineReducers({
   cityIOdataState: cityIOdataSliceReducer,
   menuState: menuSliceReducer,
   editorMenuState: editorMenuSliceReducer,
-  animationState: animationSliceReducer,
 });
