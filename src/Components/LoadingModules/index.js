8e7c94675f509993a751adfc39925eff3a0493eb
Ariel Noyman <arielnoyman@gmail.com>
2022-09-28 15:38:58 -0400

    WIP rendered view

--- src/Components/LoadingModules/index.js
+++ src/Components/LoadingModules/index.js
@@ -2,31 +2,32 @@ import { Typography, Box } from "@mui/material";
 import LinearProgress from "@mui/material/LinearProgress";
 
 const LoadingModules = (props) => {
-  const { loadingModules } = props;
+  const { loadingModules, verticalPosition, barHeight } = props;
   return (
     <>
       {loadingModules.map((module, index) => {
         return (
-       
-            <Box
-              key={"box_"+module}
-              sx={{
-                position: "fixed",
-                bottom: (index + 1) * 10,
-                width: "100%",
-                zIndex: 10000,
-              }}
+          <Box
+            key={"box_" + module}
+            sx={{
+              position: "fixed",
+              bottom: verticalPosition === "bottom" ? (index + 1) * 10 : "auto",
+              width: "100%",
+              zIndex: 10000,
+            }}
+          >
+            <LinearProgress
+              sx={{ height: barHeight || 10 }}
+              color={"secondary"}
+            />
+            <Typography
+              variant="caption"
+              color="primary"
+              style={{ marginRight: "1vw" }}
             >
-              <LinearProgress color={"secondary"} />
-              <Typography
-                variant="caption"
-                color="primary"
-                style={{ marginRight: "1vw" }}
-              >
-                Loading {module}
-              </Typography>
-            </Box>
-       
+              Loading {module}
+            </Typography>
+          </Box>
         );
       })}
     </>
--- src/views/RenderedView/RenderedViewMap.js
+++ src/views/RenderedView/RenderedViewMap.js
@@ -6,7 +6,7 @@
  * 
  */
 
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
 import DeckGL from "deck.gl";
 import { SimpleMeshLayer } from "@deck.gl/mesh-layers";
@@ -16,19 +16,40 @@ import { CubeGeometry } from "@luma.gl/engine";
 import { mapSettings } from "../../settings/settings";
 import Map from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { Button, TextField } from "@mui/material";
+import LoadingModules from "../../Components/LoadingModules";
+import {
+  Button,
+  CircularProgress,
+  TextField,
+  Box,
+  List,
+  ListItem,
+  Container,
+  Typography,
+  Grid,
+  Paper,
+  Stack,
+} from "@mui/material";
+
 import axios from "axios";
 
 const cube = new CubeGeometry({ type: "x,z", xlen: 0, ylen: 0, zlen: 0 });
 
 export default function RenderedViewMap() {
+  // fix deck view rotate
+  useEffect(() => {
+    document.addEventListener("contextmenu", (evt) => evt.preventDefault());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const refMap = useRef();
   const refDeckgl = useRef();
-  const containerDivRef = useRef();
   const [image, setImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [prompt, setPrompt] = useState(
     "Aerial view of the MIT Campus in Cambridge, Massachusetts, with new buildings and development in the center. Realistic. Accurate. Sunset with long shadows. Beautiful."
   );
+  const [userSeed, setUserSeed] = useState(1024);
 
   const cityIOdata = useSelector((state) => state.cityIOdataState.cityIOdata);
   const GEOGRID = processGridData(cityIOdata);
@@ -57,6 +78,61 @@ export default function RenderedViewMap() {
 
   const [mergeCanvas] = useState(document.createElement("canvas"));
 
+  const handleCapture = () => {
+    if (!refMap.current || !refDeckgl.current) {
+      return;
+    }
+    setIsLoading(true);
+    const mapGL = refMap.current.getMap();
+    const deck = refDeckgl.current.deck;
+    const mapboxCanvas = mapGL.getCanvas();
+    deck.redraw(true);
+    const deckglCanvas = deck.canvas;
+    mergeCanvas.width = mapboxCanvas.width;
+    mergeCanvas.height = mapboxCanvas.height;
+    var context = mergeCanvas.getContext("2d");
+    context.globalAlpha = 1.0;
+    context.drawImage(mapboxCanvas, 0, 0);
+    context.globalAlpha = 1.0;
+    context.drawImage(deckglCanvas, 0, 0);
+    const jpegFile = mergeCanvas.toDataURL("image/jpeg");
+    setImage(jpegFile);
+    mergeCanvas.toBlob(async (blob) => {
+      var formData = new FormData();
+      formData.append("image", blob, "image.jpg");
+      formData.append("prompt", prompt);
+      formData.append("user_seed", userSeed);
+      formData.append("from", "frontend");
+      await axios({
+        method: "post",
+        "Access-Control-Allow-Origin": "*",
+        mode: "no-cors",
+        url: "http://18.27.78.190:8080/",
+        data: formData,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        responseType: "blob",
+      })
+        .then(async (res) => {
+          let dataUrl = await blobToDataUrl(res.data);
+          setImage(dataUrl);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            console.log(error.request);
+          } else {
+            console.log("Error", error.message);
+          }
+        });
+    });
+  };
+
   const blobToDataUrl = (blob) => {
     return new Promise((r) => {
       let a = new FileReader();
@@ -67,156 +143,222 @@ export default function RenderedViewMap() {
 
   return (
     <>
-      <Button
-        variant="outlined"
-        onClick={() => {
-          if (!refMap.current || !refDeckgl.current) {
-            return;
-          }
-          const mapGL = refMap.current.getMap();
-          const deck = refDeckgl.current.deck;
-          const mapboxCanvas = mapGL.getCanvas();
+      {isLoading && <LoadingModules loadingModules={[""]} />}
 
-          deck.redraw(true);
-          const deckglCanvas = deck.canvas;
-
-          mergeCanvas.width = mapboxCanvas.width;
-          mergeCanvas.height = mapboxCanvas.height;
-
-          var context = mergeCanvas.getContext("2d");
-
-          context.globalAlpha = 1.0;
-          context.drawImage(mapboxCanvas, 0, 0);
-          context.globalAlpha = 1.0;
-          context.drawImage(deckglCanvas, 0, 0);
-
-          const jpegFile = mergeCanvas.toDataURL("image/jpeg");
-          setImage(jpegFile);
-
-          mergeCanvas.toBlob(async (blob) => {
-            var formData = new FormData();
-            formData.append("image", blob, "image.jpg");
-            formData.append("prompt", prompt);
-            formData.append("from", "frontend");
-
-            await axios({
-              method: "post",
-              "Access-Control-Allow-Origin": "*",
-              mode: "no-cors",
-              url: "http://18.27.78.190:8080/",
-              data: formData,
-              headers: {
-                "Content-Type": "multipart/form-data",
-              },
-              responseType: "blob",
-            })
-              .then(async (res) => {
-                let dataUrl = await blobToDataUrl(res.data);
-                setImage(dataUrl);
-              })
-              .catch((error) => {
-                if (error.response) {
-                  console.log(error.response.data);
-                  console.log(error.response.status);
-                  console.log(error.response.headers);
-                } else if (error.request) {
-                  console.log(error.request);
-                } else {
-                  console.log("Error", error.message);
-                }
-              });
-          });
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          height: "100vh",
+          overflow: "auto",
         }}
       >
-        Capture
-      </Button>
-      <TextField
-        id="outlined-basic"
-        label={prompt}
-        variant="outlined"
-        // on change, update the state
-        onChange={(e) => setPrompt(e.target.value)}
-      />
+        <Container maxWidth="xl" sx={{ mt: 5, mb: 5 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Grid
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <Paper
+                  variant="outlined"
+                  sx={{
+                    variant: "outlined",
+                    p: 2,
+                    flexDirection: "column",
+                    display: "flex",
+                    flexGrow: 1,
+                  }}
+                >
+                  <Grid container spacing={2}>
+                    <Grid item>
+                      <Typography variant="h2">DeepScope 2.0</Typography>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                      <Typography>
+                        DeepScope attempts to substitute common practices of
+                        urban design with a machine-learnt, generative
+                        visualization approach. By implementing 'Stable
+                        diffusion', an open-source Transformer model, this tool
+                        allows for real-time prototyping and visualizations of
+                        urban design processes.
+                      </Typography>
+                    </Grid>
 
-      {/* a small div  in the top left corner, above all the rest, that displays the image  */}
-      <div
-        style={{
-          position: "absolute",
-          top: 100,
-          right: 0,
-          zIndex: 1000,
-  
-        }}
-      >
-        {image && (
-          <img width="500vw" height="500vh" src={image} alt="screenshot" />
-        )}
-      </div>
+                    <Grid item xs={12} md={6}>
+                      <TextField
+                        fullWidth
+                        id="outlined-basic"
+                        required
+                        label="Text Prompt Description of the Scene"
+                        defaultValue={prompt}
+                        variant="outlined"
+                        onChange={(e) => setPrompt(e.target.value)}
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                      <TextField
+                        fullWidth
+                        id="outlined-basic"
+                        InputProps={{
+                          inputProps: {
+                            max: 10000,
+                            min: 0,
+                          },
+                        }}
+                        label="Select a random seed (0-10000)"
+                        defaultValue={userSeed}
+                        variant="outlined"
+                        type="number"
+                        onChange={(e) => setUserSeed(e.target.value)}
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                      <Button
+                        fullWidth
+                        color="secondary"
+                        variant="outlined"
+                        onClick={handleCapture}
+                        disabled={isLoading}
+                      >
+                        {!isLoading && "Capture & Render"}
+                        {isLoading && (
+                          <>
+                            <CircularProgress size={14} />
+                            <Typography sx={{ p: 1 }}>
+                              Rendering captured view... [can take ~15 seconds]
+                            </Typography>
+                          </>
+                        )}
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Paper>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} md={6} lg={6}>
+              <Paper
+                variant="outlined"
+                sx={{
+                  variant: "outlined",
+                  p: 2,
+                  flexDirection: "column",
+                  display: "flex",
+                }}
+              >
+                <Stack spacing={2} direction="column">
+                  <Typography variant="h4">CityScope Model View</Typography>
+                  <Box
+                    sx={{
+                      height: "576px",
+                      minHeight: "576px",
+                      width: "auto",
+                      position: "relative",
+                    }}
+                  >
+                    <DeckGL
+                      ref={refDeckgl}
+                      viewState={viewState}
+                      onViewStateChange={({ viewState }) =>
+                        setViewState(viewState)
+                      }
+                      controller={{
+                        touchZoom: true,
+                        touchRotate: true,
+                        keyboard: false,
+                      }}
+                      layers={[
+                        new SimpleMeshLayer({
+                          id: "mesh-layer",
+                          data: GEOGRID.features,
+                          loaders: [OBJLoader],
+                          mesh: cube,
 
-      <div
-        ref={containerDivRef}
-        style={{
-          height: "50vh",
-          width: "50vw",
-          position: "relative",
-        }}
-      >
-        <DeckGL
-          ref={refDeckgl}
-          viewState={viewState}
-          onViewStateChange={({ viewState }) => setViewState(viewState)}
-          controller={{
-            touchZoom: true,
-            touchRotate: true,
-            keyboard: false,
-          }}
-          layers={[
-            new SimpleMeshLayer({
-              id: "mesh-layer",
-              data: GEOGRID.features,
-              loaders: [OBJLoader],
-              mesh: cube,
+                          getPosition: (d) => {
+                            const pntArr = d.geometry.coordinates[0];
+                            const first = pntArr[1];
+                            const last = pntArr[pntArr.length - 2];
+                            const center = [
+                              (first[0] + last[0]) / 2,
+                              (first[1] + last[1]) / 2,
+                              // add the height of the grid cell
+                              d.properties.height.length > 1
+                                ? d.properties.height[1] > 1
+                                  ? d.properties.height[1] / 2
+                                  : 1
+                                : d.properties.height > 1
+                                ? d.properties.height / 2
+                                : 1,
+                            ];
+                            return center;
+                          },
+                          getColor: (d) =>
+                            d.properties.interactive
+                              ? d.properties.color
+                              : [0, 0, 0, 0],
+                          opacity: 0.75,
+                          getOrientation: (d) => [-180, header.rotation, -90],
+                          getScale: (d) => [
+                            GEOGRID.properties.header.cellSize / 2,
+                            d.properties.height.length > 1
+                              ? d.properties.height[1] > 1
+                                ? d.properties.height[1] / 2
+                                : 1
+                              : d.properties.height > 1
+                              ? d.properties.height / 2
+                              : 1,
+                            GEOGRID.properties.header.cellSize / 2,
+                          ],
 
-              getPosition: (d) => {
-                const pntArr = d.geometry.coordinates[0];
-                const first = pntArr[1];
-                const last = pntArr[pntArr.length - 2];
-                const center = [
-                  (first[0] + last[0]) / 2,
-                  (first[1] + last[1]) / 2,
-                  // add the height of the grid cell
-                  d.properties.height.length > 1
-                    ? d.properties.height[1]
-                    : d.properties.height,
-                ];
-                return center;
-              },
-              getColor: (d) =>
-                d.properties.interactive ? d.properties.color : [0, 0, 0, 0],
-              opacity: 0.75,
-              getOrientation: (d) => [-180, header.rotation, -90],
-              getScale: (d) => [
-                GEOGRID.properties.header.cellSize / 2,
-                d.properties.height.length > 1
-                  ? d.properties.height[1] 
-                  : d.properties.height,
-                GEOGRID.properties.header.cellSize / 2,
-              ],
+                          updateTriggers: {
+                            getScale: GEOGRID,
+                          },
+                        }),
+                      ]}
+                    >
+                      <Map
+                        ref={refMap}
+                        preserveDrawingBuffer={true} //! This is critical to preserve the WebGL drawing buffer
+                        mapboxApiAccessToken={
+                          process.env.REACT_APP_MAPBOX_TOKEN
+                        }
+                        mapStyle={mapSettings.map.mapStyle.normal}
+                      />
+                    </DeckGL>
+                  </Box>
+                </Stack>
+              </Paper>
+            </Grid>
 
-              updateTriggers: {
-                getScale: GEOGRID,
-              },
-            }),
-          ]}
-        >
-          <Map
-            ref={refMap}
-            preserveDrawingBuffer={true} //! This is critical to preserve the WebGL drawing buffer
-            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-            mapStyle={mapSettings.map.mapStyle.normal}
-          />
-        </DeckGL>
-      </div>
+            <Grid item xs={12} md={6} lg={6}>
+              <Paper
+                variant="outlined"
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "auto",
+                  width: "auto",
+                }}
+              >
+                <Stack spacing={2} direction="column">
+                  <Typography variant="h4">Captured & Rendered View</Typography>
+
+                  {image && (
+                    <img
+                      style={{ height: "576px" }}
+                      src={image}
+                      alt="screenshot"
+                    />
+                  )}
+                </Stack>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
     </>
   );
 }
