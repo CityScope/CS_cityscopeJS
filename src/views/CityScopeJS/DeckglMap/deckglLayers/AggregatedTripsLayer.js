6ba7e682eefc127a3717cd0af28cfb379146aa2c
Ariel Noyman <arielnoyman@gmail.com>
2022-10-20 15:37:49 -0400

    WIP remove agg. trip transition. Might solve #159

--- src/views/CityScopeJS/DeckglMap/deckglLayers/AggregatedTripsLayer.js
+++ src/views/CityScopeJS/DeckglMap/deckglLayers/AggregatedTripsLayer.js
@@ -2,28 +2,22 @@ import { PathLayer } from "@deck.gl/layers";
 import { hexToRgb } from "../../../../utils/utils";
 
 export default function AggregatedTripsLayer({ data, selected, opacity }) {
-  if (data.ABM2) {
-    return new PathLayer({
-      id: "AGGREGATED_TRIPS",
-      shadowEnabled: false,
-      data: data.ABM2.trips,
-      getPath: (d) => {
-        if (d.mode === selected) {
-          return d.path;
-        }
-      },
-      getColor: (d) => hexToRgb(data.ABM2.attr.mode[d.mode].color),
+  return new PathLayer({
+    id: "AGGREGATED_TRIPS",
+    shadowEnabled: false,
+    data: data?.ABM2?.trips,
+    getPath: (d) => {
+      if (d.mode === selected) {
+        return d.path;
+      }
+    },
+    getColor: (d) => hexToRgb(data.ABM2.attr.mode[d.mode].color),
 
-      opacity,
-      getWidth: 5,
+    opacity,
+    getWidth: 3,
 
-      updateTriggers: {
-        getPath: selected,
-      },
-      transitions: {
-        getColor: 500,
-        getPath: 500,
-      },
-    });
-  }
+    updateTriggers: {
+      getPath: selected,
+    },
+  });
 }
