a04120d176b0d895a3bd37d9e23afeade3deb238
Ariel Noyman <arielnoyman@gmail.com>
2022-10-23 14:00:03 -0400

    fix equality rule

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -133,7 +133,7 @@ export const computeMidGridCell = (cityIOdata) => {
 export function hslToRgb(h, s, l) {
   var r, g, b;
 
-  if (s == 0) {
+  if (s === 0) {
     r = g = b = l; // achromatic
   } else {
     function hue2rgb(p, q, t) {
