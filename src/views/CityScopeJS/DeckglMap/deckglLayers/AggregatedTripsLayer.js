08a4caedbc1a81f984bdfbe05b81f4d9effd58f7
Ariel Noyman <arielnoyman@gmail.com>
2022-12-10 23:29:46 -0500

    WIP arch layer for aggr. trips

--- src/views/CityScopeJS/DeckglMap/deckglLayers/AggregatedTripsLayer.js
+++ src/views/CityScopeJS/DeckglMap/deckglLayers/AggregatedTripsLayer.js
@@ -1,23 +1,58 @@
-import { PathLayer } from "@deck.gl/layers";
+// import { PathLayer } from "@deck.gl/layers";
+// import { hexToRgb } from "../../../../utils/utils";
+
+// export default function AggregatedTripsLayer({ data, selected, opacity }) {
+//   return new PathLayer({
+//     id: "AGGREGATED_TRIPS",
+//     shadowEnabled: false,
+//     data: data?.ABM2?.trips,
+//     getPath: (d) => {
+//       if (d.mode === selected) {
+//         return d.path;
+//       }
+//     },
+//     getColor: (d) => hexToRgb(data.ABM2.attr.mode[d.mode].color),
+
+//     opacity,
+//     getWidth: 3,
+
+//     updateTriggers: {
+//       getPath: selected,
+//     },
+//   });
+// }
+
+import { ArcLayer } from "@deck.gl/layers";
 import { hexToRgb } from "../../../../utils/utils";
 
 export default function AggregatedTripsLayer({ data, selected, opacity }) {
-  return new PathLayer({
+  return new ArcLayer({
     id: "AGGREGATED_TRIPS",
     shadowEnabled: false,
     data: data?.ABM2?.trips,
-    getPath: (d) => {
+    // on each trip in data.ABM2.trips get the first coordinate of the path for getSourcePosition
+
+    getSourcePosition: (d) => {
       if (d.mode === selected) {
-        return d.path;
+        return d.path[0];
       }
     },
-    getColor: (d) => hexToRgb(data.ABM2.attr.mode[d.mode].color),
+    // on each trip in data.ABM2.trips get the last coordinate of the path for getTargetPosition
+    getTargetPosition: (d) => {
+      if (d.mode === selected) {
+        return d.path[d.path.length - 1];
+      }
+    },
 
-    opacity,
-    getWidth: 3,
+    getSourceColor: (d) => hexToRgb(data.ABM2.attr.mode[d.mode].color),
+    getTargetColor: (d) => hexToRgb(data.ABM2.attr.mode[d.mode].color),
+
+    opacity:0.85,
+    getWidth: 2 * opacity,
 
     updateTriggers: {
-      getPath: selected,
+      getSourcePosition: selected,
+      getTargetPosition: selected,
     },
   });
 }
