246112912b01264b3aadc237a7a1ed3031dce99a
Ariel Noyman <arielnoyman@gmail.com>
2023-01-19 11:36:05 -0500

    heatmaps => grid

--- src/views/CityScopeJS/DeckglMap/deckglLayers/AccessLayer.js
+++ src/views/CityScopeJS/DeckglMap/deckglLayers/AccessLayer.js
@@ -1,29 +1,80 @@
-import { HeatmapLayer } from "deck.gl";
+import { HeatmapLayer, SimpleMeshLayer } from "deck.gl";
+import { CubeGeometry } from "@luma.gl/engine";
+import { OBJLoader } from "@loaders.gl/obj";
+import { numberToColorHsl } from "../../../../utils/utils";
 
 export default function AccessLayer({ data, intensity, selected }) {
   const accessData = data.access && data.access.features;
+  const cube = new CubeGeometry({ type: "x,z", xlen: 0, ylen: 0, zlen: 0 });
+  const header = data.GEOGRID.properties.header;
+  // const colors = [
+  //   [255, 0, 0],
+  //   [255, 167, 0],
+  //   [255, 244, 0],
+  //   [163, 255, 0],
+  //   [44, 186, 0],
+  // ];
+  // return new HeatmapLayer({
+  //   id: "ACCESS",
+  //   colorRange: colors,
+  //   debounceTimeout: 800,
+  //   radiusPixels: intensity || 35,
+  //   intensity: 1,
+  //   weightsTextureSize: 1024,
+  //   threshold: 0.3,
 
-  const colors = [
-    [255, 0, 0],
-    [255, 167, 0],
-    [255, 244, 0],
-    [163, 255, 0],
-    [44, 186, 0],
-  ];
-  return new HeatmapLayer({
+  //   data: accessData && accessData,
+  //   getPosition: (d) => d.geometry.coordinates,
+  //   getWeight: (d) => d.properties[selected],
+  //   updateTriggers: {
+  //     getWeight: selected,
+  //   },
+  // });
+
+  return new SimpleMeshLayer({
     id: "ACCESS",
-    colorRange: colors,
-    debounceTimeout: 800,
-    radiusPixels: intensity || 35,
-    intensity: 1,
-    weightsTextureSize: 1024,
-    threshold: 0.3,
+    opacity: intensity / 100 || 0.5,
 
     data: accessData && accessData,
-    getPosition: (d) => d.geometry.coordinates,
-    getWeight: (d) => d.properties[selected],
+    loaders: [OBJLoader],
+    mesh: cube,
+    getPosition: (d) => [
+      d.geometry.coordinates[0],
+      d.geometry.coordinates[1],
+      1,
+    ],
+
+    getColor: (d) => {
+      const selectedWeight = (w) => {
+        if (w > 0 && w < 1) {
+          return w;
+        } else if (w > 1) {
+          return 1;
+        } else {
+          return 0;
+        }
+      };
+      const rgb = numberToColorHsl(
+        selectedWeight(d.properties[selected]),
+        0,
+        1
+      );
+
+      return [rgb[0], rgb[1], rgb[2], 150];
+    },
+
+    getOrientation: (d) => [-180, header.rotation, -90],
+    getScale: (d) => [
+      data.GEOGRID.properties.header.cellSize / 2,
+      1 + d.properties[selected] * 100,
+      data.GEOGRID.properties.header.cellSize / 2,
+    ],
     updateTriggers: {
-      getWeight: selected,
+      getScale: selected,
+    },
+    transitions: {
+      getColor: 500,
+      getScale: 500,
     },
   });
 }
